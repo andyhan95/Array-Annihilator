1b748e09f64c7cd1f66bd3ab061786dfb7cb9f29
andyhan <andyhan95@gmail.com>
2019-10-16 17:43:24 -0700

    Wrote code to declare a 2D array and specified several future tasks.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
-    // Program goes here!
+    public static void main(String args[]) {
+        // Initialize all "global" variables here.
+        int numberOfEnemies = 0;
+
+        // Retrieve the initial quantity of enemies from the user.
+        System.out.println("Array Annihilator helps you keep track of various enemies and their remaining hit points.");
+        System.out.println("Initializing first 2D array of enemies and their attributes...\nHow many enemies should we start with?");
+        Scanner numberOfEnemiesScanner = new Scanner(System.in);
+        numberOfEnemies = numberOfEnemiesScanner.nextInt();
+
+        // Create the 2D array that will store the enemies and their attributes.
+        int[][] enemyAttributes = new int[numberOfEnemies][3]; // The three attributes are: ID, hp, and defense multiplier
+
+        // Using nested loops, retrieve the enemy hp and defense multiplier one-by-one from the user.
+
+        // Begin the main combat loop in which users can deal slash or poke damage to an enemy of their choice.
+
+        // Introduce an "escape key" that allows users to see all enemy's stats in a tabular format.
+    }
 }
